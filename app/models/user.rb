981bc01e23bd64eefb3940ae49f83425a36c3a3c
hidaxphoenix <zeecombu2115@gmail.com>
2020-02-08 21:43:02 +0900

    ログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  # devise実装時に追記。空の文字列入力を防ぐ目的。
+end

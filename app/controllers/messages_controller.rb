30ff8f905832a3ab57f979e90dbda9306fdc9e50
hidaxphoenix <zeecombu2115@gmail.com>
2020-02-13 15:45:06 +0900

    チャット画面自動更新 1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,8 +10,9 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.json
-      end
+      format.html { redirect_to group_messages_path(@group) }
+      format.json
+    end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
